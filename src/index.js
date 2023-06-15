d5bc118d18ba78a89e19b6303aa9ada5940b51d2
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-06-14 22:22:07 -0500

    Auth and Post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,16 +8,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     // <React.StrictMode>
-        <Auth0Provider
-            domain="mollemira.us.auth0.com"
-            clientId="cmBKMx5fz4Qqe6Kd40F5S8K6fXJhD3l1"
-            authorizationParams={{
-                redirect_uri: window.location.origin,
-                audience:'https://mollemira.us.auth0.com/api/v2/',
-                scope:'openid profile email'
-            }}
-        >
-            <App />
-        </Auth0Provider>
+<Auth0Provider
+    domain="mollemira.us.auth0.com"
+    clientId="cmBKMx5fz4Qqe6Kd40F5S8K6fXJhD3l1"
+    redirectUri={window.location.origin}
+    audience="https://mollemira.us.auth0.com/api/v2/"
+    scope="openid profile email"
+>
+    <App />
+</Auth0Provider>
+
     //{/* </React.StrictMode> */}
 );
